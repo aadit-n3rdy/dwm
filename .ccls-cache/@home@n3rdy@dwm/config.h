f4ec787c502837b7628fc73aeede0bd788c6e2ca
aadit-n3rdy <eaadithyaa@gmail.com>
2021-04-14 14:33:08 +0530

    Latest

--- .ccls-cache/@home@n3rdy@dwm/config.h
+++ .ccls-cache/@home@n3rdy@dwm/config.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 6;        /* gaps between windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
+static const unsigned int gappx     = 15;        /* gaps between windows */
 static const unsigned int snap      = 0;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
@@ -11,21 +11,29 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "mononoki Nerd Font Mono:size=10" };
+static const char dmenufont[]       = "mononoki Nerd Font Mono:size=10";
+
+static const char bg[] = "#282a36";
+static const char col_curline[] = "#44475a";
+static const char fg[] = "#f8f8f2";
+static const char comment[] = "#6272a4";
+static const char cyan[] = "#8be9fd";
+static const char green[] = "#50fa7b";
+static const char orange[] = "#ffb86c";
+static const char pink[] = "#ff79c6";
+static const char red[] = "#ff5555";
+static const char yellow[] = "#f1fa8c";
+static const char purple[] = "#bd93f9";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { fg,        bg,       bg },
+	[SchemeSel]  = { bg,     purple, purple },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "dev", "web", "comms", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -35,6 +43,9 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "discord",  NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "zoom",     NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Conky",    NULL,       NULL,       0,            1,           -1 }
 };
 
 /* layout(s) */
@@ -65,7 +76,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
